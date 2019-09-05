9fb7368beeb0d1a2a90a64019f56f7a544c429e4
Tobias <tobias.ohlsson123@gmail.com>
2019-09-05 18:28:38 +0530

    Initial commit

---
+++ tsp.py
@@ -0,0 +1,69 @@
+import fileinput
+import random
+
+import numpy as np
+
+is_euclidean = False
+number_of_cities = 0
+cities = []
+
+for i, line in enumerate(fileinput.input()):
+    line = line.rstrip()
+    if i == 0:
+        is_euclidean = (line == 'euclidean')
+    elif i == 1:
+        number_of_cities = int(line)
+        distance_matrix = np.zeros((number_of_cities, number_of_cities))
+    elif i < (number_of_cities + 2):
+        c = line.split()
+        cities.append((float(c[0]), float(c[1])))
+    elif i < (2 * number_of_cities + 2):
+        s = line.split()
+        a = i - (number_of_cities+2)
+        for j, d in enumerate(s):
+            distance_matrix[a][j] = d
+
+
+front_node = random.randint(0, number_of_cities)
+back_node = front_node
+
+
+def create_tsp(front_node, back_node, matrix):
+    tour = str(front_node)
+    total_distance = 0
+    for k in range(number_of_cities):
+        min_position_front = [0, 0]
+        min_position_back = [0, 0]
+        min_value_front = float("inf")
+        min_value_back = float("inf")
+        for i in range(number_of_cities):
+            if (matrix[front_node][i] < min_value_front) & (matrix[front_node][i] != -1) & (front_node != i):
+                min_value_front = matrix[front_node][i]
+                min_position_front = i
+            if (matrix[back_node][i] < min_value_back) & (matrix[back_node][i] != -1) & (back_node != i):
+                min_value_back = matrix[i][back_node]
+                min_position_back = i
+        if min_value_front < min_value_back:
+            matrix[:, front_node] = -1
+            tour = tour + " " + str(min_position_front)
+            front_node = min_position_front
+            total_distance += min_value_front
+        else:
+            matrix[:, back_node] = -1
+            tour = str(min_position_back) + " " + tour
+            back_node = min_position_back
+            total_distance += min_value_back
+    return tour, total_distance
+
+
+tour, score = create_tsp(front_node, back_node, distance_matrix)
+print(tour)
+
+
+
+
+
+
+
+
+
